80618e9469dc744abe04d51ef9d9f91a3d43bb63
Julien Maille <julien.maille@gmail.com>
2022-09-30 22:12:47 +0200

    show selling and gifting fees

--- costs.js
+++ costs.js
@@ -4,7 +4,7 @@ async function fetchEthPrice() {
   });
 };
 
-async function fetchContractFee() {
+async function fetchContractSellingFee() {
   return await fetch("https://alpha-mainnet.starknet.io/feeder_gateway/estimate_fee?blockNumber=pending", {
   "headers": {
       "accept": "*/*",
@@ -23,6 +23,31 @@ async function fetchContractFee() {
     "mode": "cors",
     "credentials": "omit"
   }).then(response => {
+    return response.json();
+  });
+};
+
+async function fetchContractGiftingFee() {
+  return await fetch("https://alpha-mainnet.starknet.io/feeder_gateway/estimate_fee?blockNumber=pending", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US;q=0.9,en;q=0.8",
+    "content-type": "application/json",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://rules.art/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": `{"contract_address":"0x50930d64e6984b652bdef862685f9d9942b53a7b18005ba0b742921af825405",
+  "entry_point_selector":"0x15d40a3d6ca2ac30f4031e42be28da9b056fef9bb7357ac5e85627ee876e5ad",
+      "calldata":["1","2000031840833493536440815036539786941974305919381618969872893526239637868210","730327702935662484951878015260342994925824754530470619774470754812327453764","0","8","8","2277802928010336623191910023386536307813376284777533236402445017574182900741","1658174508219360922237481747078004012752963661731002166998323232047434917121","21167561166113709653962090351","45838324932415289422458381450346496","1","0","1","0","11"],
+  "version":"0x100000000000000000000000000000000",
+  "signature":["640398736397574976213236397041857234454759463438144240912583448179824017547","2620693834791730664836136802575589559860839142135458871387170359813876185954"]}`,
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+}).then(response => {
     return response.json();
   });
 };
