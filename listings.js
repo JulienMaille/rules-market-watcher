639a827bc09a687fd5dde87bb7bb46ed7665bbd8
Julien Maille <julien.maille@gmail.com>
2022-10-02 22:46:58 +0200

    new page for past transfers

--- listings.js
+++ listings.js
@@ -23,3 +23,29 @@ async function fetchOffers(filter) {
     return response.json();
   });
 };
+
+async function fetchTransfers(filter) {
+  return await fetch("https://js76hmpzh9-dsn.algolia.net/1/indexes/transfers-date-desc/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.13.0)%3B%20Browser", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US;q=0.9,en;q=0.8",
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      "x-algolia-api-key": "c7d162df9bb45f258af878219300d8c5",
+      "x-algolia-application-id": "JS76HMPZH9"
+    },
+    "body": `{"query":"",
+      "facetFilters":["is_sale:true"],
+      "filters": "${filter}",
+      "page":0,
+      "hitsPerPage":1000
+    }`,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+  }).then(response => {
+    return response.json();
+  });
+};
