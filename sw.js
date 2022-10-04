fe4834a3e265b47f00bdbf7de555712a1f4b52d4
Julien Maille <julien.maille@gmail.com>
2022-10-04 18:09:15 +0200

    remove broken action in notification

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('notificationclick', function(event) {
-  switch(event.action) {
-    case 'open_url':
-    clients.openWindow(event.notification.body);
-    break;
-  }
-}
-, false);
