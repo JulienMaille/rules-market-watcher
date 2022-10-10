4f684000e6dad6ba54b9e57c7dc465a0794a85b9
Julien Maille <julien.maille@gmail.com>
2022-10-10 22:06:42 +0200

    wip: show user cards

--- listings.js
+++ listings.js
@@ -21,7 +21,7 @@ async function fetchOffers(filter) {
   }).then(response => {
     return response.json();
   });
-};
+}
 
 async function fetchTransfers(filter, facetFilter="is_sale:true") {
   return await fetch("https://js76hmpzh9-dsn.algolia.net/1/indexes/transfers-date-desc/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.13.0)%3B%20Browser", {
@@ -47,14 +47,14 @@ async function fetchTransfers(filter, facetFilter="is_sale:true") {
   }).then(response => {
     return response.json();
   });
-};
+}
 
 function cleanQuery(q) {
   return JSON.stringify(q).replace(/\"/g, '\\\"');
 }
 
 async function fetchUserId(username) {
-  username = cleanQuery(username);
+  var username = cleanQuery(username);
   return await fetch("https://rules-t8gf.onrender.com/https://api.rules.art/graphql", {
     "headers": {
       "accept": "*/*",
@@ -70,7 +70,7 @@ async function fetchUserId(username) {
 }
 
 async function fetchUserNames(ids) {
-  idsString = cleanQuery(ids);
+  var idsString = cleanQuery(ids);
   return await fetch("https://rules-t8gf.onrender.com/https://api.rules.art/graphql", {
     "headers": {
       "accept": "*/*",
@@ -78,9 +78,50 @@ async function fetchUserNames(ids) {
       "content-type": "application/json",
     },
     "body": `{
-      "operationName": null,
-      "variables": {},
       "query": "{ usersByIds(ids: ${idsString}) { id, username, slug }}"
+    }`,
+    "method": "POST",
+    "mode": "cors"
+  }).then(response => {
+    return response.json();
+  });
+}
+
+async function fetchRoster(userId) {
+  return await fetch("https://js76hmpzh9-dsn.algolia.net/1/indexes/cards-date-desc/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.13.0)%3B%20Browser", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US;q=0.9,en;q=0.8",
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      "x-algolia-api-key": "c7d162df9bb45f258af878219300d8c5",
+      "x-algolia-application-id": "JS76HMPZH9"
+    },
+    "body": `{"query":"",
+      "facetFilters":["ownerUserId:${userId}"],
+      "page":0,
+      "hitsPerPage":1000
+    }`,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+  }).then(response => {
+    return response.json();
+  });
+}
+
+async function fetchUserCards(ids) {
+  var idsString = cleanQuery(ids);
+  return await fetch("https://rules-t8gf.onrender.com/https://api.rules.art/graphql", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US;q=0.9,en;q=0.8",
+      "content-type": "application/json",
+    },
+    "body": `{
+      "query": "{ cardsByIds(ids: ${idsString}) {serialNumber, cardModel {id} }}"
     }`,
     "method": "POST",
     "mode": "cors"
