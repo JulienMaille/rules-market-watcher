2df68b4f45413b968ac8c4ae830dd018a5ddc8fe
Julien Maille <julien.maille@data-pixel.com>
2022-10-10 18:46:17 +0200

    script to export card list from graphql

---
+++ export-cards.cmd
@@ -0,0 +1,18 @@
+echo var allCardModels = > "cards.js"
+
+curl "https://rules-t8gf.onrender.com/https://api.rules.art/graphql" ^
+  -H "accept: */*" ^
+  -H "content-type: application/json" ^
+  -H "origin: null" ^
+  --data-raw "{ \"query\": \" { ^
+    allCardModels { ^
+      id ^
+      slug ^
+      artist { displayName, slug } ^
+      season ^ ^
+      scarcity { name } ^
+      pictureUrl ^
+    } ^
+  } \" }" >> "cards.js"
+
+echo var topTier = ["le-reglement", "caballero", "jeanjass", "deen-burbigo", "zuukou-mayzie", "winnterzuko", "kerchak"]; >> "cards.js"
