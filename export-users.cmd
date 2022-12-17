7cecd8e9a666cbba73ac546005ebf7553f2c3f5c
Julien Maille <julien.maille@gmail.com>
2022-12-17 22:38:16 +0100

    UPD stat tools

--- export-users.cmd
+++ export-users.cmd
@@ -6,4 +6,4 @@ curl "https://api.rules.art/graphql" ^
   -H "Connection: 'keep-alive" ^
   -H "DNT: 1" ^
   -H "Origin: https://api.rules.art" ^
-  --data-raw "{\"query\":\"{users(slugs:[]){id}}\"}" >> "js/users.js"
+  --data-raw "{\"query\":\"{users(slugs:[]){starknetWallet{address}}}\"}" >> "js/users.js"
